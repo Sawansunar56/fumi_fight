a37064aa25176b86e1a503ba50a300c8bb648e81
Sawan Sunar <sawansunar24072002@gmail.com>
2024-08-30 23:19:11 +0530

    just changed something in the build

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ set glad_include=/I..\vendor\glad\include
 set glad_src=..\vendor\glad\src\glad.c
 
 set win_libs=shell32.lib opengl32.lib user32.lib gdi32.lib
-set vendor_dir=/LIBPATH:"..\vendor\glfw\lib-vc2022"
+set vendor_dir=/LIBPATH:"..\vendor\glfw\lib"
 set vendor_libs=glfw3.lib
 
 set includes=/I..\src /I..\vendor\glfw\include /I..\vendor\glad\include
@@ -17,7 +17,6 @@ set includes=/I..\src /I..\vendor\glfw\include /I..\vendor\glad\include
 if not exist .\build mkdir .\build
 pushd .\build
 
-ctime -begin stupid_time.ctm
 if not exist glad.obj (
     echo "[LIBRARY_COMPILLING] Compilling Glad..."
     cl /MD /Z7 %glad_include% /c %glad_src%
@@ -26,6 +25,5 @@ if not exist glad.obj (
 cl %comp_flags% %includes% /c %files% 
 link /OUT:main.exe *.obj %link_flags% %vendor_dir%  %win_libs% %vendor_libs%
 
-ctime -end stupid_time.ctm
 popd 
 
